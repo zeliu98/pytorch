8856952d02be23846a3774594cdf68c7b9fb773e
jiej <jiej@nvidia.com>
2020-12-08 15:41:42 -0800

    refactor profiling optional
    ghstack-source-id: d0e502370212115abcbc7d8d1d12d9d518cd49e7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/47667

--- torch/csrc/jit/ir/ir.cpp
+++ torch/csrc/jit/ir/ir.cpp
@@ -2043,16 +2043,6 @@ Node* ProfileOp::allocNewInstance(Graph* g) {
   return new ProfileOp(g, {nullptr});
 }
 
-void ProfileOptionalOp::cloneFrom(Node* other_) {
-  Node::cloneFrom(other_);
-  auto other = other_->cast<ProfileOptionalOp>();
-  this->callback_ = other->getCallback();
-}
-
-Node* ProfileOptionalOp::allocNewInstance(Graph* g) {
-  return new ProfileOptionalOp(g, {nullptr});
-}
-
 void ProfileIValueOp::cloneFrom(Node* other_) {
   Node::cloneFrom(other_);
   auto other = other_->cast<ProfileIValueOp>();
@@ -2072,7 +2062,6 @@ TypePtr NamedValue::type() const {
 }
 
 const Symbol ProfileOp::Kind = ::c10::prim::profile;
-const Symbol ProfileOptionalOp::Kind = ::c10::prim::profile_optional;
 const Symbol ProfileIValueOp::Kind = ::c10::prim::profile_ivalue;
 
 OperatorSet::OperatorSet(std::initializer_list<const char*> sig_literals) {
