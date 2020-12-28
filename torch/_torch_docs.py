c0362983457e6fbedab247d062bf96e5944f8e4e
ErjiaGuan <erjia@fb.com>
2020-12-28 15:18:12 -0500

    Fix remainder type promotion (Fix Windows type promotion)
    ghstack-source-id: 62064932b3295ee616e0fc62f218058e5da7276b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48668

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -7186,17 +7186,17 @@ Computes the element-wise remainder of division.
 The dividend and divisor may contain both for integer and floating point
 numbers. The remainder has the same sign as the divisor :attr:`other`.
 
-When :attr:`other` is a tensor, the shapes of :attr:`input` and
-:attr:`other` must be :ref:`broadcastable <broadcasting-semantics>`.
+Supports :ref:`broadcasting to a common shape <broadcasting-semantics>`,
+:ref:`type promotion <type-promotion-doc>`, and integer and float inputs.
 
 Note:
     Complex inputs are not supported. In some cases, it is not mathematically
     possible to satisfy the definition of a modulo operation with complex numbers.
+    Check the note from :func:`torch.fmod` for zero divisor.
 
 Args:
     input (Tensor): the dividend
-    other (Tensor or float): the divisor that may be either a number or a
-                               Tensor of the same shape as the dividend
+    other (Tensor or float/integer): the divisor, which may be either a number or a tensor
 
 Keyword args:
     {out}
@@ -7205,7 +7205,7 @@ Example::
 
     >>> torch.remainder(torch.tensor([-3., -2, -1, 1, 2, 3]), 2)
     tensor([ 1.,  0.,  1.,  1.,  0.,  1.])
-    >>> torch.remainder(torch.tensor([1., 2, 3, 4, 5]), 1.5)
+    >>> torch.remainder(torch.tensor([1, 2, 3, 4, 5]), 1.5)
     tensor([ 1.0000,  0.5000,  0.0000,  1.0000,  0.5000])
 
 .. seealso::
